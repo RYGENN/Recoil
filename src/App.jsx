a2010ede84e38196c37fe858e287877ad33a1bc6
anupam <anupammahato357@gmail.com>
2024-07-12 15:08:44 +0530

    atomFamily

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,109 @@
 import { useContext, useState } from 'react'
-import './App.css'
-import { countAtom, evenSelector } from './store/atoms/count'
-import { useRecoilValue,useRecoilState, RecoilRoot,useSetRecoilState } from 'recoil'
+import { todosAtom  } from './store/atoms/count'
+import { useRecoilValue,useRecoilState, RecoilRoot,useSetRecoilState, useRecoilValueLoadable } from 'recoil'
 
 function App() {
-   
+ 
   return (
-   
     <RecoilRoot>
-      <Count/>
+    
+        <TodoComponent id={1}/>
+        <TodoComponent id={2}/>
     </RecoilRoot>
   
   )
 }
 
 
-function Count(){
-  console.log("redering from Count")
-  return (
-    <>
-      <CounterRenderer/>
-      <Buttons />
-    </>
-  )
-}
+// function MainComponent(){
+//   const networkCount = useRecoilValue(networkCountAtom);
+//   const jobCount = useRecoilValue(jobCountAtom);
+//   const messageCount = useRecoilValue(messageCountAtom);
+//   const notificationCount = useRecoilValue(notificationCountAtom);
+//   const setNetworkCount = useSetRecoilState(networkCountAtom);
+//   const sumOfNotifications = useRecoilValue(sumOfNotificationsAll);
 
-function CounterRenderer(){
-  console.log("redering from CounterRenderer")
-  const count = useRecoilValue(countAtom)
-  return (
-    <>
-      <h1>{count}</h1>
-      <EvenCounterRenderer/>
-    </>
-  )
-}
+//   function increaseNetworkCount(){
+//     setNetworkCount(networkCount => networkCount+1)
+//   }
+ 
 
-function EvenCounterRenderer(){
-  console.log("redering from EvenCounter")
-   const isEven = useRecoilValue(evenSelector);
-   console.log(isEven)
-  return (
-    <div>
-       {!isEven ? "It is Even" : null}
+//   return(
+//     <div>
+//         <button>Home</button>
+//         <button>My Network {networkCount}</button>
+//         <button>Jobs {jobCount}</button>
+//         <button>Messaging {messageCount}</button>
+//         <button>Notifications {notificationCount}</button>
+//         <button onClick={increaseNetworkCount}>Increase Network Count</button>
+//         <button>Profile {sumOfNotifications}</button>
+//     </div>
+//   )
+// }
+
+
+function TodoComponent({id}){
+  const todo = useRecoilValueLoadable(todosAtom(id));
+  console.log(todo) // due to Loadable now this todo is a object with two properties contents and state 
+
+  if(todo.state === "loading"){   // <suspense fallback="Loading"><suspense> used in recoilValue not in Loadable 
+    return <div>
+      loading...
     </div>
-  )
+  } else{
+    return(
+      <div>
+        <h1>{todo.contents.id}</h1>
+        <h1>{todo.contents.todo}</h1>
+      </div>
+    )
+  }
+ 
 }
 
-function Buttons(){
-  console.log("redering from Buttons")
- const setCount = useSetRecoilState(countAtom) // This will stop button re-render
-  return(
-    <>
-      <button onClick={()=>{setCount(count => count+1)}} style={{padding:4 ,margin:4}}>Increase</button>
-      <button onClick={()=>{setCount(count => count-1)}} style={{padding:4 ,margin:4}}>Decrease</button>
-    </>
-  )
-}
+
+
+// function Count(){
+//   console.log("redering from Count")
+//   return (
+//     <>
+//       <CounterRenderer/>
+//       <Buttons />
+//     </>
+//   )
+// }
+
+// function CounterRenderer(){
+//   console.log("redering from CounterRenderer")
+//   const count = useRecoilValue(countAtom)
+//   return (
+//     <>
+//       <h1>{count}</h1>
+//       <EvenCounterRenderer/>
+//     </>
+//   )
+// }
+
+// function EvenCounterRenderer(){
+//   console.log("redering from EvenCounter")
+//    const isEven = useRecoilValue(evenSelector);
+//    console.log(isEven)
+//   return (
+//     <div>
+//        {!isEven ? "It is Even" : null}
+//     </div>
+//   )
+// }
+
+// function Buttons(){
+//   console.log("redering from Buttons")
+//  const setCount = useSetRecoilState(countAtom) // This will stop button re-render
+//   return(
+//     <>
+//       <button onClick={()=>{setCount(count => count+1)}} style={{padding:4 ,margin:4}}>Increase</button>
+//       <button onClick={()=>{setCount(count => count-1)}} style={{padding:4 ,margin:4}}>Decrease</button>
+//     </>
+//   )
+// }
 
 export default App
--- src/store/atoms/count.jsx
+++ src/store/atoms/count.jsx
@@ -1,17 +1,64 @@
-import {atom, selector} from "recoil";
+import {atom, atomFamily, selector, selectorFamily} from "recoil";
+import axios from "axios";
 //atom
-export const countAtom = atom({
-    key:"countAtom",
-    default:0
-});
+// export const countAtom = atom({
+//     key:"countAtom",
+//     default:0
+// });
 
-// selector
-export const evenSelector = selector({
-    key:"evenSelector",
-    get:({get}) =>{
-        const count = get(countAtom);
-        return count % 2;
-    }
+// // selector
+// export const evenSelector = selector({
+//     key:"evenSelector",
+//     get:({get}) =>{
+//         const count = get(countAtom);
+//         return count % 2;
+//     }
+// })
+
+// state management 
+
+// export const networkCountAtom = atom({
+//     key:"networkCount",
+//     default:20
+// })
+
+// export const jobCountAtom = atom({
+//     key:"jobCount",
+//     default:0
+// })
+
+// export const messageCountAtom = atom({
+//     key:"messageCount",
+//     default:5
+// })
+
+// export const notificationCountAtom = atom({
+//     key:"notificationCount",
+//     default:45
+// })
+
+// export const sumOfNotificationsAll = selector({
+//     key:"sumOfNotificationsAll",
+//     get:({get}) =>{
+//         const networkCount = get(networkCountAtom);
+//         const jobCount = get(jobCountAtom);
+//         const messageCount = get(messageCountAtom);
+//         const notificationCount = get(notificationCountAtom);
+//         return networkCount + jobCount + messageCount + notificationCount;
+//     }
+// })
+
+export const todosAtom = atomFamily({
+    key:"todosAtomFamily",
+    default:selectorFamily({  // we are making asynchronous calls to the backend for everytodo so we need to use selectorfamily
+        key:"todosAtomSelectorFamily",
+        get: function(id){
+         return  ( async ({get})=>{
+                await new Promise(r => setTimeout(r,3000))
+                const res  = await axios.get(`https://dummyjson.com/todos/${id}`);
+                return res.data;
+
+            })
+        }
+    })
 })
-
-// statae management
